75beb7d8fbdc6d86f48a66226683a0eb13b31141
nndang <nndang.sc@gmail.com>
2025-08-26 22:21:30 +0700

    feat(domain): errors domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get('ping')
-    getHello() {
+    ping() {
         return this.appService.ping();
     }
 }
---
+++ src/core/domain/errors.ts
@@ -0,0 +1,59 @@
+/**
+ * DomainError is a custom error class for domain layer
+ */
+export class DomainError extends Error {
+    public readonly publicMessage: string;
+    public readonly privateMessage: string;
+    public readonly type: ErrorType;
+
+    public readonly metadata?: Record<string, any>;
+
+    constructor(
+        type: ErrorType,
+        publicMessage: string,
+        privateMessage?: string,
+        metadata?: Record<string, any>,
+    ) {
+        super(privateMessage || publicMessage);
+        this.name = 'DomainError';
+
+        this.type = type;
+        this.publicMessage = publicMessage;
+        this.privateMessage = privateMessage || publicMessage;
+        this.metadata = metadata;
+    }
+
+    toString() {
+        return `${this.type}: ${this.privateMessage}`;
+    }
+
+    toPublicString() {
+        return `${this.type}: ${this.publicMessage}`;
+    }
+
+    is(type: ErrorType | DomainError) {
+        if (type instanceof DomainError) {
+            return this.type === type.type;
+        }
+        return this.type === type;
+    }
+}
+
+/**
+ * ErrorType is an enumeration of error types
+ */
+export enum ErrorType {
+    // General Errors
+    ErrDataExists = 'DataExists',
+    ErrInternal = 'Internal',
+    ErrInvalidData = 'InvalidData',
+    ErrDataConflicting = 'DataConflicting',
+    ErrDataVersionConflict = 'DataVersionConflict',
+    ErrNotFound = 'NotFound',
+    ErrNoDataUpdated = 'NoDataUpdated',
+    ErrForBidden = 'Forbidden',
+    ErrValidation = 'Validation',
+    ErrDatabase = 'Database',
+    ErrUnauthorized = 'Unauthorized',
+    ErrUnknown = 'Unknown',
+}
